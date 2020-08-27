dc15b3569b6e0688771d018f2262fcecc73b2500
Rinat Levchuk <rinat.levchuk@gmail.com>
2020-08-27 23:20:49 +0700

    8

--- pandoc_plantuml_filter.py
+++ pandoc_plantuml_filter.py
@@ -49,7 +49,7 @@ def plantuml(key, value, format_, _):
                 with open(src, "wb") as f:
                     f.write(txt)
 
-                sys.stderr.write(" ".join(PLANTUML_BIN.split() + ["-t" + filetype, src]))
+                subprocess.check_call(" ".join(["java", "-jar", PLANTUML_BIN.split(), "-t", filetype, src]))
                 # subprocess.check_call(PLANTUML_BIN.split() +
                 #                      ["-t" + filetype, src])
                 sys.stderr.write('Created image ' + dest + '\n')
